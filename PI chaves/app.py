e1f93095583e2042b24c24e2b9a3ae869bcf3171
Bryan <mattosbryan14@gmail.com>
2026-08-13 20:59:16 -0300

    primeiro

---
+++ PI chaves/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from sqlalchemy import create_engine, Column, Integer, String, Numeric, Date
+from sqlalchemy.orm import declarative_base, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+from tabelas import engine, Base, Chave, Usuario, Pessoa, Ambiente, Movimentacao, Reserva, Movimentacao_devolucao
+
+
+#cria a sesao
+Session = sessionmaker(bind=engine)
+sessao  = Session()
+
+
+app = Flask(__name__)
+app.secret_key="123456"
+
+#home
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+#chave
+@app.route("/chave")
+def chave():
+    return render_template('chave.html')
+
+
+#ambiente
+@app.route("/ambiente")
+def ambiente():
+    return render_template('ambiente.html')
+
+#usuario
+@app.route("/usuario")
+def perfil():
+    return render_template('usuario.html')
+
+
+#movimentacao
+@app.route("/movimentacao")
+def movimentacao():
+    return render_template('movimentacao.html')
+
+#reserva
+@app.route("/reserva")
+def reserva():
+    return render_template('reserva.html')
+
+#historico
+@app.route("/historico")
+def historico():
+    return render_template('historico.html')
+
+
+app.run(debug=True)
---
+++ PI chaves/tabelas.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from sqlalchemy import create_engine, Column, Integer, String, Numeric, Date, Boolean, Time
+from sqlalchemy.orm import declarative_base, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+
+#Criando conexao com o banco de dados
+engine = create_engine(
+    "mysql+pymysql://root:@localhost:3306/db_chave",
+    echo=True
+)
+
+#Criando tabela banco de dados chave
+Base=declarative_base()
+class Chave(Base):
+    __tablename__ = "tb_chave"
+    id_chave      = Column(Integer, primary_key=True)
+    identificador = Column(String(250))
+    id_ambiente   = Column(Integer)
+    observacao    = Column(String(250))
+    disponivel    = Column(Boolean)
+Base.metadata.create_all(engine)
+
+
+#Criando tabela banco de dados usuarios
+class Usuario(Base):
+    __tablename__ = "tb_usuario"
+    id_usuario    = Column(Integer, primary_key=True)
+    nome_usuario  = Column(String(250))
+    email         = Column(String(250))
+    senha         = Column(Numeric(15,0))
+    perfil        = Column(String(150))
+Base.metadata.create_all(engine)
+
+#Criando tabela banco de dados pessoa
+class Pessoa(Base):
+    __tablename__ = "tb_pessoa"
+    id_pessoa     = Column(Integer, primary_key=True)
+    nome_pessoa   = Column(String(200), nullable=True)
+    matricula     = Column(Numeric(12,0), nullable=True)
+    cargo         = Column(String(250), nullable=True)
+Base.metadata.create_all(engine)
+
+#Criando tabela banco de dados ambiente
+class Ambiente(Base):
+    __tablename__       = "tb_ambiente"
+    id_ambiente         = Column(Integer, primary_key=True)
+    ambiente            = Column(String(150), nullable=True)
+    disponivel_ambiente = Column(Boolean)
+    observacao_ambiente = Column(String(250))
+Base.metadata.create_all(engine)
+
+
+#Criando tabela banco de dados movimentacao
+class Movimentacao(Base):
+    __tablename__      = "tb_movimentacao"
+    id_movimentacao    = Column(Integer, primary_key=True)
+    id_chave           = Column(Integer)
+    id_pessoa          = Column(Integer)
+    data_retirada      = Column(Date)
+    id_reserva         = Column(Integer)
+    horario_inicio     = Column(Time)
+Base.metadata.create_all(engine)
+
+
+#Criando tabela banco de dados reserva
+class Reserva(Base):
+    __tablename__   = "tb_reserva"
+    id_reserva      = Column(Integer, primary_key=True)
+    id_chave        = Column(Integer)
+    id_ambiente     = Column(Integer)
+    id_usuario      = Column(Integer)
+    data_reserva    = Column(Date)
+    horario_reserva = Column(Time)
+Base.metadata.create_all(engine)
+
+
+#Criando tabela banco de dados movimentacao_devolucao
+class Movimentacao_devolucao(Base):
+    __tablename__       = "tb_movimentacao_devolucao"
+    id_devolucao        = Column(Integer, primary_key=True)
+    id_reserva          = Column(Integer)
+    date_reserva        = Column(Date)
+    horario_devolucao   = Column(Time)
+    obsevacao_devolucao = Column(String(250))
+Base.metadata.create_all(engine)
+
